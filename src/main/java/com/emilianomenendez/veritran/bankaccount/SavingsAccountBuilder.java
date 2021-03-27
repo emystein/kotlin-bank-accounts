1d8bd9fa6a1d337b32a871fac31c90f280bade63
Emiliano Menéndez <emystein@gmail.com>
2021-03-27 10:14:48 -0300

    Renamed ZeroLowerLimit to BalanceLimit.

--- src/main/java/com/emilianomenendez/veritran/bankaccount/SavingsAccountBuilder.java
+++ src/main/java/com/emilianomenendez/veritran/bankaccount/SavingsAccountBuilder.java
@@ -2,13 +2,13 @@ package com.emilianomenendez.veritran.bankaccount;
 
 import com.emilianomenendez.veritran.Customer;
 import com.emilianomenendez.veritran.bankaccount.withdraw.WithdrawLimit;
-import com.emilianomenendez.veritran.bankaccount.withdraw.ZeroLowerLimit;
+import com.emilianomenendez.veritran.bankaccount.withdraw.BalanceLimit;
 import com.emilianomenendez.veritran.money.Dollars;
 
 public class SavingsAccountBuilder {
     private final Customer accountOwner;
     private Dollars initialBalance;
-    private WithdrawLimit withdrawLimit = new ZeroLowerLimit();
+    private WithdrawLimit withdrawLimit = new BalanceLimit();
 
     public SavingsAccountBuilder(Customer accountOwner) {
         this.accountOwner = accountOwner;
--- src/main/java/com/emilianomenendez/veritran/bankaccount/withdraw/BalanceLimit.java
+++ src/main/java/com/emilianomenendez/veritran/bankaccount/withdraw/BalanceLimit.java
@@ -3,7 +3,7 @@ package com.emilianomenendez.veritran.bankaccount.withdraw;
 import com.emilianomenendez.veritran.bankaccount.Balance;
 import com.emilianomenendez.veritran.money.Dollars;
 
-public class ZeroLowerLimit implements WithdrawLimit {
+public class BalanceLimit implements WithdrawLimit {
     @Override
     public boolean reached(Balance balance, Dollars amountToWithdraw) {
         return balance.isLessThan(amountToWithdraw);
