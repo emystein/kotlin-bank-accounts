fd83a68c2e485a79d82839bc6aaa93de95dda5b7
Emiliano Menéndez <emystein@gmail.com>
2021-04-02 14:26:43 -0300

    Minor cleanup

--- src/main/java/com/emilianomenendez/veritran/Customer.java
+++ src/main/java/com/emilianomenendez/veritran/Customer.java
@@ -1,28 +1,14 @@
 package com.emilianomenendez.veritran;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 
+@EqualsAndHashCode
+@RequiredArgsConstructor
 public class Customer {
     private final String name;
 
     public static Customer named(String name) {
         return new Customer(name);
-    }
-
-    public Customer(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return name.equals(customer.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
