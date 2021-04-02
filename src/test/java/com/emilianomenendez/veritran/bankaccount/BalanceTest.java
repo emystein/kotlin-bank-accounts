c6afb540b5e6dc339d97c6f70879e6ac5c5049aa
Emiliano Menéndez <emystein@gmail.com>
2021-04-02 14:18:12 -0300

    Clean-up BalanceTest.

--- src/test/java/com/emilianomenendez/veritran/bankaccount/BalanceTest.java
+++ src/test/java/com/emilianomenendez/veritran/bankaccount/BalanceTest.java
@@ -2,59 +2,38 @@ package com.emilianomenendez.veritran.bankaccount;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BalanceTest {
     @Test
-    void positiveBalanceShouldNotAcceptNegativeAmount() {
+    void givenNegativeAmountWhenCreatePositiveBalanceThenItShouldNotBeCreated() {
         assertThrows(IllegalArgumentException.class, () -> new PositiveBalance("USD", -10));
     }
 
     @Test
-    void positiveBalanceShouldIncludePositiveSignAndCurrencyAndAmount() {
+    void givenPositiveAmountWhenCreatePositiveBalanceThenItShouldHavePositiveAmount() {
         var balance = new PositiveBalance("USD", 10);
 
-        assertEquals("USD", balance.getCurrency());
-        assertEquals(10, balance.getAmount());
+        assertBalance(balance, "USD", 10);
     }
 
     @Test
-    void negativeBalanceShouldIncludeNegativeSignAndCurrencyAndAmount() {
+    void givenPositiveAmountWhenCreateNegativeBalanceThenItShouldHaveNegativeAmount() {
         var balance = new NegativeBalance("USD", 10);
 
-        assertEquals("USD", balance.getCurrency());
-        assertEquals(-10, balance.getAmount());
+        assertBalance(balance, "USD", -10);
     }
 
     @Test
-    void PositiveBalance100UsdShouldBeGreaterThanPositiveBalance10USD() {
-        var balance100 = new PositiveBalance("USD", 100);
-        var balance10 = new PositiveBalance("USD", 10);
+    void givenNegativeAmountWhenCreateNegativeBalanceThenItShouldHaveNegativeAmount() {
+        var balance = new NegativeBalance("USD", -10);
 
-        assertTrue(balance100.isGreaterThanOrEqual(balance10));
+        assertBalance(balance, "USD", -10);
     }
 
-    @Test
-    void PositiveBalance100UsdShouldBeGreaterThanNegativeBalance10USD() {
-        var balance100 = new PositiveBalance("USD", 100);
-        var balanceMinus10 = new NegativeBalance("USD", 10);
-
-        assertTrue(balance100.isGreaterThanOrEqual(balanceMinus10));
-    }
-
-    @Test
-    void NegativeBalance10UsdShouldBeLowerThanPositiveBalance10USD() {
-        var balanceMinus10 = new NegativeBalance("USD", 10);
-        var balance10 = new PositiveBalance("USD", 10);
-
-        assertTrue(balanceMinus10.isLessThan(balance10));
-    }
-
-    @Test
-    void NegativeBalance100UsdShouldBeLowerThanNegativeBalance10USD() {
-        var balanceMinus100 = new NegativeBalance("USD", 100);
-        var balanceMinus10 = new NegativeBalance("USD", 10);
-
-        assertTrue(balanceMinus100.isLessThan(balanceMinus10));
+    private void assertBalance(Balance balance, String expectedCurrency, int expectedAmount) {
+        assertEquals(expectedCurrency, balance.getCurrency());
+        assertEquals(expectedAmount, balance.getAmount());
     }
 }
