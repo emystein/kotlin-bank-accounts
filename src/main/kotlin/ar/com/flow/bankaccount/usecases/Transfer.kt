10e39f0f0bbb57fb95aaef43dc85cc415d5836d1
Emiliano Menéndez <emystein@gmail.com>
2021-09-05 17:40:20 -0300

    Clean-up Transfer use case.

--- src/main/kotlin/ar/com/flow/bankaccount/usecases/Transfer.kt
+++ src/main/kotlin/ar/com/flow/bankaccount/usecases/Transfer.kt
@@ -1,17 +1,30 @@
 package ar.com.flow.bankaccount.usecases
 
+import ar.com.flow.bankaccount.domain.BankAccount
+import ar.com.flow.bankaccount.domain.SavingsAccount
 import ar.com.flow.bankaccount.ports.out.BankAccounts
 import ar.com.flow.bankaccount.ports.out.Customers
 import ar.com.flow.money.Money
 
 class Transfer(private val customers: Customers, private val bankAccounts: BankAccounts) {
     fun transfer(debitCustomerName: String, currency: String, amountToTransfer: Int, creditCustomerName: String) {
-        val debitCustomer = customers.customerNamed(debitCustomerName)
-        val debitAccount = bankAccounts.accountOwnedBy(debitCustomer, currency)
-        val creditCustomer = customers.customerNamed(creditCustomerName)
-        val creditAccount = bankAccounts.accountOwnedBy(creditCustomer, currency)
-        debitAccount.transfer(Money(debitAccount.currency, amountToTransfer), creditAccount)
-        bankAccounts.save(debitAccount)
-        bankAccounts.save(creditAccount)
+        val (debitAccount, creditAccount) = accountsOwnedBy(debitCustomerName, creditCustomerName, currency = currency)
+
+        debitAccount.transfer(Money(currency, amountToTransfer), creditAccount)
+
+        saveChanges(debitAccount, creditAccount)
+    }
+
+    private fun accountsOwnedBy(vararg ownerNames: String, currency: String): List<SavingsAccount> {
+        return ownerNames.map { ownerName -> accountOwnedBy(ownerName, currency) }
+    }
+
+    private fun accountOwnedBy(ownerName: String, currency: String): SavingsAccount {
+        val accountOwner = customers.customerNamed(ownerName)
+        return bankAccounts.accountOwnedBy(accountOwner, currency)
+    }
+
+    private fun saveChanges(vararg accounts: SavingsAccount) {
+        accounts.forEach { account -> bankAccounts.save(account) }
     }
 }
