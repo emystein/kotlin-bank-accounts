67a8227837ded6cc47e9da1fe98c27e0789d2cff
Emiliano Menéndez <emystein@gmail.com>
2021-09-07 14:19:40 -0300

    Clean-up SavingsAccountMapper.

--- src/main/kotlin/ar/com/flow/bankaccount/adapters/out/persistence/jpa/SavingsAccountMapper.kt
+++ src/main/kotlin/ar/com/flow/bankaccount/adapters/out/persistence/jpa/SavingsAccountMapper.kt
@@ -7,26 +7,28 @@ class SavingsAccountMapper(
     private val customerRepository: CustomerRepository,
     private val bankAccountRepository: BankAccountRepository
 ) {
+    private val customerMapper = CustomerMapper()
+
     fun toDomain(bankAccount: ar.com.flow.bankaccount.adapters.out.persistence.jpa.BankAccount): SavingsAccount {
-        val owner = CustomerMapper().toDomain(bankAccount.owner)
+        val owner = customerMapper.toDomain(bankAccount.owner)
         return SavingsAccount(owner, bankAccount.currency)
     }
 
     fun toJpa(bankAccount: BankAccount): ar.com.flow.bankaccount.adapters.out.persistence.jpa.BankAccount {
         val customerName = bankAccount.owner.name
-        val maybeJpaOwner = customerRepository.findByName(customerName)
-        val jpaOwner: Customer = if (maybeJpaOwner.isPresent) {
-            maybeJpaOwner.get()
+        val maybeOwner = customerRepository.findByName(customerName)
+        val owner = if (maybeOwner.isPresent) {
+            maybeOwner.get()
         } else {
             customerRepository.save(Customer(0, customerName))
         }
 
         val currency = bankAccount.currency
-        val maybeJpaBankAccount = bankAccountRepository.findByOwnerAndCurrency(jpaOwner, currency)
-        return if (maybeJpaBankAccount.isPresent) {
-            maybeJpaBankAccount.get()
+        val maybeBankAccount = bankAccountRepository.findByOwnerAndCurrency(owner, currency)
+        return if (maybeBankAccount.isPresent) {
+            maybeBankAccount.get()
         } else {
-            bankAccountRepository.save(BankAccount(0L, jpaOwner, currency))
+            bankAccountRepository.save(BankAccount(0L, owner, currency))
         }
     }
 }
