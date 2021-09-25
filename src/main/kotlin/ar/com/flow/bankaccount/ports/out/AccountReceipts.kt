8634c17741f5db050aa50fe03a0934ce966b95bd
Emiliano Menéndez <emystein@gmail.com>
2021-09-25 09:49:29 -0300

    Add @Throws annotation to AccountReceipts::add

--- src/main/kotlin/ar/com/flow/bankaccount/ports/out/AccountReceipts.kt
+++ src/main/kotlin/ar/com/flow/bankaccount/ports/out/AccountReceipts.kt
@@ -1,11 +1,14 @@
 package ar.com.flow.bankaccount.ports.out
 
 import ar.com.flow.bankaccount.domain.AccountId
+import ar.com.flow.bankaccount.domain.DifferentAccountException
 import ar.com.flow.bankaccount.domain.transaction.receipt.Receipt
 
 interface AccountReceipts {
     val accountId: AccountId
+
     fun all(): List<Receipt>
-    // TODO: add precondition of receipt account owner and currency must match this instance accountOwner and currency
+
+    @Throws(DifferentAccountException::class)
     fun add(receipt: Receipt)
 }
