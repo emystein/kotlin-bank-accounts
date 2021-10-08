2066d72d2f2cfb3b54ae26eec3c9accbcff238b5
Emiliano Menéndez <emystein@gmail.com>
2021-10-07 21:05:30 -0300

    Add API Docs.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Run
+```bash
+gradle bootRun
+```
+
+
+# API
+
+## API Docs
+
+http://localhost:8080/v3/api-docs/
+
+## Swagger UI
+
+http://localhost:8080/swagger-ui.html
--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,13 @@ repositories {
 }
 
 dependencies {
+	implementation(kotlin("stdlib-jdk8"))
+	implementation(kotlin("reflect"))
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.assertj:assertj-core:3.20.2")
 	testImplementation("io.mockk:mockk:1.12.0")
